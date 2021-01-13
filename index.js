2d59a77eeee823f94090d700858f2cf6102e7655
Muna-kim <63827788+Muna-kim@users.noreply.github.com>
2021-01-14 00:45:44 +0900

    #14 로그아웃 기능

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post('/api/users/login',(req, res) => {
   })
 })
 
-app.post('/api/users/auth', auth, (req, res) => {
+app.get('/api/users/auth', auth, (req, res) => {
 
   res.status(200).json({
     _id: req.user._id,
@@ -81,6 +81,15 @@ app.post('/api/users/auth', auth, (req, res) => {
     image: req.user.imate
   })
 })
-
+ app.get('/api/users/logout', auth, (req, res) => {
+   User.findOneAndUpdate({_id: req.user._id},
+    {token:""}
+    ,(err, user) =>{
+      if(err) return res.json({success:false, err})
+      return res.status(200).send({
+        success: true
+      })
+    })
+ })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
